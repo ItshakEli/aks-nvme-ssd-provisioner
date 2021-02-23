959806fcd8162e107fef255c8660728f2a9a8b37
Itsik Hillel <Itsik.Hillel@edr.dom>
2021-02-23 07:57:30 +0200

    modified dockerfile & removed nvme_init.sh

--- aks-nvme-ssd-provisioner.sh
+++ aks-nvme-ssd-provisioner.sh
@@ -3,53 +3,37 @@
 set -o errexit
 set -o nounset
 set -o pipefail
+#set -x 
 
-SSD_NVME_DEVICE_LIST=($(ls /sys/block | grep nvme | xargs -I. echo /dev/. || true))
-SSD_NVME_DEVICE_COUNT=${#SSD_NVME_DEVICE_LIST[@]}
-RAID_DEVICE=${RAID_DEVICE:-/dev/md0}
-RAID_CHUNK_SIZE=${RAID_CHUNK_SIZE:-512}  # Kilo Bytes
-FILESYSTEM_BLOCK_SIZE=${FILESYSTEM_BLOCK_SIZE:-4096}  # Bytes
-STRIDE=$(expr $RAID_CHUNK_SIZE \* 1024 / $FILESYSTEM_BLOCK_SIZE || true)
-STRIPE_WIDTH=$(expr $SSD_NVME_DEVICE_COUNT \* $STRIDE || true)
+NVME_DEVICE_NAME=${NVME_DEVICE_NAME:-nvme}
+LOGICAL_VOLUME_GROUP=${LOGICAL_VOLUME_GROUP:-es_data}
+LOGICAL_VOLUME=${LOGICAL_VOLUME:-es_data}
+LOGICAL_VOLUME_COUNT=2
+SSD_NVME_DEVICE_LIST=($(ls /sys/block | grep $NVME_DEVICE_NAME | xargs -I. echo /dev/. || true))
 
 
-# Checking if provisioning already happend
-if [[ "$(ls -A /pv-disks)" ]]
+if [ -d  "/dev/$LOGICAL_VOLUME_GROUP" ]
 then
-  echo 'Volumes already present in "/pv-disks"'
-  echo -e "\n$(ls -Al /pv-disks | tail -n +2)\n"
+  echo 'Volumes already present in "/dev/$LOGICAL_VOLUME_GROUP"'
+  echo -e "\n$(ls -Al /dev/$LOGICAL_VOLUME_GROUP | tail -n +2)\n"
   echo "I assume that provisioning already happend, doing nothing!"
   sleep infinity
 fi
 
-# Perform provisioning based on nvme device count
-case $SSD_NVME_DEVICE_COUNT in
-"0")
-  echo 'No devices found of type "Microsoft NVMe Direct Disk"'
-  echo "Maybe your node selectors are not set correct"
-  exit 1
-  ;;
-"1")
-  mkfs.ext4 -m 0 -b $FILESYSTEM_BLOCK_SIZE $SSD_NVME_DEVICE_LIST
-  DEVICE=$SSD_NVME_DEVICE_LIST
-  ;;
-*)
-  mdadm --create --verbose $RAID_DEVICE --level=0 -c ${RAID_CHUNK_SIZE} \
-    --raid-devices=${#SSD_NVME_DEVICE_LIST[@]} ${SSD_NVME_DEVICE_LIST[*]}
-  while [ -n "$(mdadm --detail $RAID_DEVICE | grep -ioE 'State :.*resyncing')" ]; do
-    echo "Raid is resyncing.."
-    sleep 1
-  done
-  echo "Raid0 device $RAID_DEVICE has been created with disks ${SSD_NVME_DEVICE_LIST[*]}"
-  mkfs.ext4 -m 0 -b $FILESYSTEM_BLOCK_SIZE -E stride=$STRIDE,stripe-width=$STRIPE_WIDTH $RAID_DEVICE
-  DEVICE=$RAID_DEVICE
-  ;;
-esac
+echo "creating physical volumes"
+pvcreate ${SSD_NVME_DEVICE_LIST[@]}
+#pvscan
 
-UUID=$(blkid -s UUID -o value $DEVICE)
-mkdir -p /pv-disks/$UUID
-mount -o defaults,noatime,discard,nobarrier --uuid $UUID /pv-disks/$UUID
-echo "Device $DEVICE has been mounted to /pv-disks/$UUID"
-echo "NVMe SSD provisioning is done and I will go to sleep now"
+echo "creating vg $LOGICAL_VOLUME_GROUP"
+vgcreate $LOGICAL_VOLUME_GROUP ${SSD_NVME_DEVICE_LIST[@]}
+#vgdisplay
 
-#sleep infinity
+let VG_PERCENT=100/$LOGICAL_VOLUME_COUNT
+
+for i in $( seq 1 $LOGICAL_VOLUME_COUNT )
+do
+    echo "creating logical volumes $LOGICAL_VOLUME$i"
+    lvcreate -l $VG_PERCENT%VG -n $LOGICAL_VOLUME$i $LOGICAL_VOLUME_GROUP
+    echo "creating ext4 file system"
+    mkfs.ext4 /dev/$LOGICAL_VOLUME_GROUP/$LOGICAL_VOLUME$i
+done
